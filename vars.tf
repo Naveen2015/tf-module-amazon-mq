91a2fd2fb5a22b6cf1439a0ec480fdfb9ec82c42
naveen <naveenkumar9707@gmail.com>
2024-10-19 17:39:27 -0500

    rabbitmq

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "subnets" {}
+variable "name" {
+  default = "rabbitmq"
+}
+variable "tags" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+
+variable "kms_arn" {}
+variable "port_no" {
+  default = 5672
+}
+variable "instance_type" {}
+variable "bastion_cidr" {}
